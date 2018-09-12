e0c76739df4703e5a14aa9f3a32b07794f9e0eb2
Justin Calleja <calleja.justin@gmail.com>
2018-09-12 17:23:59 +0200

    Solution at end of exercise-2

--- src/App.js
+++ src/App.js
@@ -2,25 +2,46 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
   state = {
-    count: 0
+    value: 0,
+    count: 0,
+    visible: true
   };
 
-  handleClick(e) {
-    e.persist();
-    this.setState({ event: e });
-  }
+  handleClick = () => {
+    this.setState(prevState => ({ count: prevState.count + prevState.value }));
+  };
+
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({ value: +value });
+  };
+
+  handleKeyDown = event => {
+    if (event.key === "Enter") {
+      return this.handleClick();
+    }
+  };
+
+  handleVisibilityClick = () => {
+    this.setState(({ visible }) => ({ visible: !visible }));
+  };
 
   render() {
+    const { count, visible, value } = this.state;
     return (
-      <div>
+      <div className="App">
         <button onClick={this.handleClick}>+</button>
-        <pre>{this.state.event ? this.state.event.type : null}</pre>
+        <input
+          type="number"
+          value={value}
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
+        />
+        <button onClick={this.handleVisibilityClick}>
+          {visible ? "Hide" : "Show"}
+        </button>
+        {visible ? <div>Count is {count}</div> : null}
       </div>
     );
   }
