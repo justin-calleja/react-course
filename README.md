fe657da9f144c44cf9c4010cd33aa356fc62c000
Justin Calleja <calleja.justin@gmail.com>
2018-09-18 18:18:06 +0200

    End of component-lifecycle-part-1

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Contains solution at end of exercise-2.
+Contains solution at end of component-lifecycle-part-1.
--- src/App.js
+++ src/App.js
@@ -1,47 +1,129 @@
 import React, { Component } from "react";
+import tippy from "tippy.js";
+import axios from "axios";
+import NProgress from "nprogress";
 import "./App.css";
 
-class App extends Component {
+class StopWatch extends Component {
   state = {
-    value: 0,
-    count: 0,
-    visible: true
+    time: 0
   };
 
-  handleClick = () => {
-    this.setState(prevState => ({ count: prevState.count + prevState.value }));
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(state => ({
+        time: state.time + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return <div>{this.state.time}s</div>;
+  }
+}
+
+class Repos extends Component {
+  state = {
+    repos: null
   };
 
-  handleChange = event => {
-    const value = event.target.value;
-    this.setState({ value: +value });
-  };
+  componentDidMount() {
+    this.fetchRepos();
+  }
 
-  handleKeyDown = event => {
-    if (event.key === "Enter") {
-      return this.handleClick();
+  // componentDidUpdate(prevProps, prevState, snapshot) {
+  //   if (prevProps.org !== this.props.org) {
+  //     this.setState({ repos: null });
+  //     this.fetchRepos();
+  //   }
+  // }
+
+  fetchRepos = async () => {
+    NProgress.start();
+    try {
+      const response = await axios.get(
+        `https://api.github.com/orgs/${this.props.org}/repos`
+      );
+      this.setState({ repos: response.data });
+    } catch (err) {
+      console.error(err.message);
     }
-  };
-
-  handleVisibilityClick = () => {
-    this.setState(({ visible }) => ({ visible: !visible }));
+    NProgress.done();
   };
 
   render() {
-    const { count, visible, value } = this.state;
+    const { repos } = this.state;
+    return (
+      <ul>
+        {repos
+          ? repos.map(repo => (
+              <li key={repo.id}>
+                <a target="_blank" href={repo.html_url}>
+                  {repo.name}
+                </a>{" "}
+              </li>
+            ))
+          : null}
+      </ul>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    selectedOrg: null,
+    stopwatchMounted: true
+  };
+
+  componentDidMount() {
+    tippy(".hello");
+  }
+
+  handleMount = () => this.setState({ stopwatchMounted: true });
+
+  handleUnmount = () => this.setState({ stopwatchMounted: false });
+
+  handleOrgChange = event => {
+    this.setState({
+      selectedOrg: event.target.value
+    });
+  };
+
+  render() {
+    const { stopwatchMounted, selectedOrg } = this.state;
     return (
       <div className="App">
-        <button onClick={this.handleClick}>+</button>
-        <input
-          type="number"
-          value={value}
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyDown}
-        />
-        <button onClick={this.handleVisibilityClick}>
-          {visible ? "Hide" : "Show"}
-        </button>
-        {visible ? <div>Count is {count}</div> : null}
+        {stopwatchMounted ? <StopWatch /> : null}
+        <button onClick={this.handleMount}>Mount StopWatch</button>
+        <button onClick={this.handleUnmount}>Unmount StopWatch</button>
+        <div title="hello tippy!" className="hello">
+          Hover over me!
+        </div>
+        <div>
+          <label htmlFor="facebook">Facebook</label>
+          <input
+            id="facebook"
+            type="radio"
+            value="facebook"
+            checked={this.state.selectedOrg === "facebook"}
+            onChange={this.handleOrgChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="node">Node.js</label>
+          <input
+            id="nodejs"
+            type="radio"
+            value="nodejs"
+            checked={this.state.selectedOrg === "nodejs"}
+            onChange={this.handleOrgChange}
+          />
+        </div>
+        {selectedOrg ? <Repos key={selectedOrg} org={selectedOrg} /> : null}
       </div>
     );
   }
